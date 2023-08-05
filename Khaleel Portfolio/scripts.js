377aaccf64c11c066c9c8cefe51cb3d3ae16c71c
Khaleelullah Sheriff <130530135+KHALEEL-21@users.noreply.github.com>
2023-08-05 14:50:33 +0530

    Add files via upload

---
+++ Khaleel Portfolio/scripts.js
@@ -0,0 +1,29 @@
+function showMsg() {
+    alert("Here Khaleel")
+  }
+  
+  function redirectToKhaleel(){
+    document.getElementById("redirectToKhaleel").addEventListener("click", function() {
+    location.href = "https://www.google.com/search?q=khaleelullah+sheriff&rlz=1C1VDKB_enIN1004IN1004&ei=-myeZP_KCoHkseMPgfuuuAU&ved=0ahUKEwj_r_GSpur_AhUBcmwGHYG9C1cQ4dUDCA8&uact=5&oq=khaleelullah+sheriff&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGtoYWxlZWx1bGxhaCBzaGVyaWZmMgUQABiiBDIFEAAYogQyBRAAGKIEMgUQABiiBDIFEAAYogRI1CVQgQhYjhhwAXgAkAEAmAH1AqAB_QmqAQUyLTQuMbgBA8gBAPgBAcICCBAAGKIEGLADwgIKECEYoAEYwwQYCuIDBBgBIEGIBgGQBgU&sclient=gws-wiz-serp"; 
+  });
+  }
+  
+  function redirectToYoutube(){
+    document.getElementById("redirectToYoutube").addEventListener("click", function() {
+    location.href = "https://www.youtube.com/c/vibezbrothers/desktop"; 
+  });
+  }
+  
+  function redirectToGitHub(){
+    document.getElementById("redirectToGitHub").addEventListener("click", function() {
+    location.href = ""; 
+  });
+  }
+  
+
+
+  function redirectToNewPage(){
+  document.getElementById("redirectToNewPage").addEventListener("click", function() {
+    location.href = "https://www.linkedin.com/in/khaleelullah-sheriff-k-947b62245"; 
+  });
+  }
